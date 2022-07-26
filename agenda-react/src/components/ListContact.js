cee03df3e0f185c78a8114f46950fcbb29251f16
jekeande <jekeande@gmail.com>
2022-07-25 22:26:59 -0300

    commit

--- agenda-react/src/components/ListContact.js
+++ agenda-react/src/components/ListContact.js
@@ -8,11 +8,17 @@ export default function ListContact() {
     const URL = "http://localhost:3004/contatos"
     const [state, setState] = useState([])
     const [updateList, setUpdateList] = useState (false)
+    const [ search, setSearch ] = useState("")
 
     const getData = async () => {
         const response = await axios.get(URL);
         return response;
     }
+
+    const searcher = (e) => {
+        setSearch(e.target.value)   
+    }
+    const results = !search ? state : state.filter((dato)=> dato.nome.toLowerCase().includes(search.toLocaleLowerCase()))
 
     useEffect(() => {
         getData().then((response) => {
@@ -21,19 +27,23 @@ export default function ListContact() {
     }, [updateList])
 
     return (
+        <div>
+            <input type="text" placeholder='Buscar Contato' value={search} onChange={searcher} className='buscar'/>
 
-        <div className="ListContact">
-                {
-                    state.map((contatos, index) => (
-                        <Contact 
-                            key={index}
-                            contatos={contatos}
-                            setUpdateList = {setUpdateList}
-                            updateList = {updateList}
-                        />
-                    ))
-                }
+            <div className="ListContact">
+                    {
+                        results.map((contatos, index) => (
+                            <Contact 
+                                key={index}
+                                contatos={contatos}
+                                setUpdateList = {setUpdateList}
+                                updateList = {updateList}
+                            />
+                        ))
+                    }
+            </div>
         </div>
+       
         
     );
 }
